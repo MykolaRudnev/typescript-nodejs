5aaef0a29976ae6c32dc9dafd17d5d80f1a240ba
Mykola Rudnev <RudnevMykola@gmail.com>
2020-04-26 20:13:03 +0200

    adding middleware and types

---
+++ src/routes/todos.ts
@@ -0,0 +1,16 @@
+import {Router}  from "express"
+
+
+const router = Router();
+
+router.post('/');
+ 
+router.get('/');
+
+router.patch('/:id');
+
+router.delete('/:id');
+
+
+
+export default router;
